d6ca809d8992c0372ae966c0a3249e683afa8b41
unknown <govalle99@gmail.com>
2023-01-19 12:15:07 -0600

    Adding operators folder

---
+++ Operators/arithmeticOperators.dart
@@ -0,0 +1,13 @@
+void main() {
+  int suma = 5 + 6 + 7 + 8;
+  double sustraccion = 5 - 6 + 7 - 5.6;
+  int multiplica = 8 * 5;
+  double division = 10 / 10;
+  double modulo = 7 % 7;
+
+  print('suma 5 + 6 + 7 + 8 de: $suma');
+  print('substraccion de 5 - 6 + 7 - 5.6: $sustraccion');
+  print('multipicacion de 8 * 5: $multiplica');
+  print('division de 10 / 10: $division');
+  print('Modulo o residuo de 7 / 7: $modulo');
+}
